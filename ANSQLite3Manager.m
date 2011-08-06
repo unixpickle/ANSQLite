9fd021fc99605ac015d810bb51d861eea9b42b5f
Alex Nichol <alexnichol@comcast.net>
2011-08-06 16:03:40 -0400

    Updated typing logic

--- ANSQLite3Manager.m
+++ ANSQLite3Manager.m
@@ -30,13 +30,14 @@ static int myCallback (void * notUsed, int argc, char * argv[], char * names[]) 
 @synthesize database;
 
 - (id)init {
-	if (self = [super init]) {
+	if ((self = [super init])) {
 		database = NULL;
 	}
 	return self;
 }
+
 - (id)initWithDatabaseFile:(NSString *)filename {
-	if (self = [super init]) {
+	if ((self = [super init])) {
 		if (![[NSFileManager defaultManager] fileExistsAtPath:filename]) {
 			FILE * fp = fopen([filename UTF8String], "w");
 			fclose(fp);
@@ -50,6 +51,7 @@ static int myCallback (void * notUsed, int argc, char * argv[], char * names[]) 
 	}
 	return self;
 }
+
 - (BOOL)openDatabaseFile:(NSString *)filename {
 	if (database) {
 		return NO;
@@ -66,73 +68,96 @@ static int myCallback (void * notUsed, int argc, char * argv[], char * names[]) 
 	}
 	else return YES;
 }
+
 - (NSArray *)executeQuery:(NSString *)query {
 	if (!database) return nil;
+	if (gReturnValue) [gReturnValue release];
 	gReturnValue = [[NSMutableArray alloc] init];
 	char * zErrMsg = NULL;
 	int rc = sqlite3_exec(database, [query UTF8String], myCallback, NULL, &zErrMsg);
 	if (rc != SQLITE_OK) {
+		[gReturnValue release];
+		gReturnValue = nil;
 		return nil;
 	}
-	return [gReturnValue autorelease];
+	NSArray * array = [NSArray arrayWithArray:gReturnValue];
+	[gReturnValue release];
+	gReturnValue = nil;
+	return array;
 }
+
 - (NSArray *)executeQuery:(NSString *)query withParameters:(NSArray *)params {
-	sqlite3_stmt * stmt;
+	if (!database) return nil;
+	sqlite3_stmt * stmt = NULL;
 	int rc = sqlite3_prepare_v2(database, [query UTF8String], [query length],
 								&stmt, NULL);
+	if (rc != SQLITE_OK) {
+		if (stmt) {
+			sqlite3_finalize(stmt);
+		}
+		return nil;
+	}
+	
 	for (int i = 0; i < [params count]; i++) {
 		id obj = [params objectAtIndex:i];
 		if ([obj isKindOfClass:[NSString class]]) {
-			sqlite3_bind_text(stmt, i+1, [(NSString *)obj UTF8String],
-							  [(NSString *)obj length], SQLITE_TRANSIENT);
+			const char * utfString = [(NSString *)obj UTF8String];
+			sqlite3_bind_text(stmt, i+1, utfString,
+							  strlen(utfString), SQLITE_TRANSIENT);
 		} else if ([obj isKindOfClass:[NSData class]]) {
 			sqlite3_bind_blob(stmt, i+1, [(NSData *)obj bytes], 
 							  [(NSData *)obj length], SQLITE_TRANSIENT);
 		} else if ([obj isKindOfClass:[NSNumber class]]) {
-			sqlite3_bind_int(stmt, i+1, [(NSNumber *)obj intValue]);
+			if ([(NSNumber *)obj doubleValue] == (double)([(NSNumber *)obj longLongValue])) {
+				sqlite3_bind_double(stmt, i+1, [(NSNumber *)obj doubleValue]);
+			} else {
+				sqlite3_bind_int64(stmt, i+1, [(NSNumber *)obj longLongValue]);
+			}
 		}
 	}
-	NSMutableArray * resultArray = [[NSMutableArray alloc] init];
 	
+	NSMutableArray * resultArray = [[NSMutableArray alloc] init];
 	while (sqlite3_step(stmt) == SQLITE_ROW) {
 		NSMutableDictionary * row = [[NSMutableDictionary alloc] init];
 		for (int i = 0; i < sqlite3_column_count(stmt); i++) {
-			const char * name = sqlite3_column_name(stmt, i);
-			char * value = (char *)sqlite3_column_blob(stmt, i);
-			if (!value) {
-				value = (char *)sqlite3_column_text(stmt, i);
+			NSString * name = [NSString stringWithUTF8String:(const char *)sqlite3_column_name(stmt, i)];
+			int type = sqlite3_column_type(stmt, i);
+			switch (type) {
+				case SQLITE_INTEGER:
+					[row setObject:[NSNumber numberWithLongLong:sqlite3_column_int64(stmt, i)] forKey:name];
+					break;
+				case SQLITE_TEXT:
+					[row setObject:[NSString stringWithUTF8String:(const char *)sqlite3_column_text(stmt, i)] forKey:name];
+					break;
+				case SQLITE_BLOB:
+				{
+					const char * blobData = sqlite3_column_blob(stmt, i);
+					int length = sqlite3_column_bytes(stmt, i);
+					[row setObject:[NSData dataWithBytes:blobData length:length] forKey:name];
+					break;
+				}
+				case SQLITE_FLOAT:
+					[row setObject:[NSNumber numberWithDouble:sqlite3_column_double(stmt, i)] forKey:name];
+					break;
 			}
-			int length = sqlite3_column_bytes(stmt, i);
-			if (!value) value = "";
-			// check for null termination
-			BOOL isNullTermed = YES;
-			for (int i = 0; i < length; i++) {
-				if (value[i] == 0) isNullTermed = NO;
-			}
-			
-			//printf("column: %s, contents: %s\n", name, value);
-			
-			if (isNullTermed)
-				[row setObject:[NSString stringWithFormat:@"%s", value] forKey:[NSString stringWithFormat:@"%s", name]];
-			else [row setObject:[NSData dataWithBytes:value length:length] forKey:[NSString stringWithFormat:@"%s", name]];
 		}
-		[resultArray addObject:[row autorelease]];
+		[resultArray addObject:row];
+		[row release];
 	}
-	sqlite3_reset(stmt);
 	sqlite3_finalize(stmt);
-	if (rc != SQLITE_OK) {
-		// there was a serious problem
-		[resultArray release];
-		return nil;
-	}
-	return [resultArray autorelease];
+	return resultArray;
 }
+
 - (void)closeDatabase {
-	sqlite3_close(database);
-	database = NULL;
+	if (database) {
+		sqlite3_close(database);
+		database = NULL;
+	}
 }
+
 - (void)dealloc {
 	if (database) [self closeDatabase];
 	[super dealloc];
 }
+
 @end
